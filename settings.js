07cf1f2975b74534472dd7ee0262d4876df18f22
emmyhenztv <emmyhenztv@gmail.com>
2026-03-06 12:39:37 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Meta Tech',
+  author: 'SHAZAM',
+  botName: "Meta MD",
+  botOwner: 'SHAZAM', // Your name
+  ownerNumber: '2349076642926', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
---
+++ token.js
@@ -0,0 +1,4 @@
+module.exports = {
+  BOT_TOKEN: '8535330157:AAE7g3hGOuRjFkIrWhWuc5HfFZFH-Wb_is8',  
+  startupPassword: 'shazam'
+};
